d51b309b36c0da9a734a87356a5c733d6805dd04
Iryna Polishchuk <i.polishchuk@lionwood.software>
2020-05-19 23:44:00 +0300

    Add icons for bottom navigation.

--- src/presentation/assets/index.js
+++ src/presentation/assets/index.js
@@ -5,3 +5,7 @@ export const financeIcon = require('./icons/finance.png');
 export const walletIcon = require('./icons/wallet.jpg');
 export const securityIcon = require('./icons/security.jpg');
 export const warsIcon = require('./icons/wars.jpeg');
+
+export const globeIcon = require('./bottomNavigation/globe.png');
+export const listIcon = require('./bottomNavigation/list.png');
+export const coinsIcon = require('./bottomNavigation/coins.png');
