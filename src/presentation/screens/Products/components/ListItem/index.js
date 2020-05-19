71505d4de17c287951ec291d043914dff07ab9ec
Iryna Polishchuk <i.polishchuk@lionwood.software>
2020-05-19 23:47:31 +0300

    Remove touch from ListItem component.

--- src/presentation/screens/Products/components/ListItem/index.js
+++ src/presentation/screens/Products/components/ListItem/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import {View, TouchableOpacity, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import {Text} from 'components';
 
 export default ({style, item, ...props}) => (
-  <TouchableOpacity
-    {...props}
-    pointerEvents={props.pointerEvents || 'box-none'}
-    style={[styles.container, style]}>
+  <View {...props} style={[styles.container, style]}>
     <Text h3>{item.title}</Text>
     <Text style={styles.subtitle}>{item.description}</Text>
     <Text style={styles.subtitle}>
       {item.price} {'   '}
       {item.url}
     </Text>
-  </TouchableOpacity>
+  </View>
 );
 
 const styles = StyleSheet.create({
